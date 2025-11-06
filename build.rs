4d461a831750ecda6dfec196d25f362e9829b5bc
Drake Boulianne <dboul63@gmail.com>
2025-11-05 21:03:39 -0700

    Initial commit

---
+++ build.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=/opt/VimbaX_2025-2/api/lib/libVmbC.so");
+    unsafe{ std::env::set_var("BINDGEN_EXTRA_CLANG_ARGS", "-I\"/opt/VimbaX_2025-2/api/include\"") };
+
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include "/opt/VimbaX_2025-2/api/include/VmbC/VmbC.h"
